160b28ab64eb1d093f1309bef5bb31fa46432bfc
Muriel Proton <muriel.proton@epitech.eu>
2020-11-04 12:13:42 +0100

    Save the file as : NameFile+Year+Month+Day.pdf The file's name never changes. And it needs to be dated so that it can be sorted by date, when searched for.

--- exemple02.js
+++ exemple02.js
@@ -1,10 +1,15 @@
-function myFunction()
-{
+function ymd(){
+    var fullDate = new Date();
+    var date = fullDate.getFullYear()+"_"+(fullDate.getMonth()+1)+"_"+fullDate.getDate();
+    return date;
+}
+function myFunction() {
+    var date = ymd();
     var doc = new jsPDF('landscape');
-    doc.text(20, 20, 'Hello world!'+ $("#nom").first().val());
+    doc.text(20, 20, 'Hello world!' + $("#nom").first().val());
     doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
     doc.addPage();
     doc.text(20, 20, 'Do you like that?');
 
-    doc.save('Test.pdf');
+    doc.save('feuille_de_caisse' + date + '.pdf');
 }
